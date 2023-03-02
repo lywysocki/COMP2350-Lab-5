e2330ca32b94fa07e80cf36e0ad097bb24e7f0a8
lywysocki <lywysocki@gmail.com>
2023-03-02 09:02:52 -0500

    stuff added

--- src/BFS.java
+++ src/BFS.java
@@ -1,2 +1,9 @@
+/**
+ *
+ * @author Laura Wysocki and Bryan DeFaria
+ * @version 1.0.0 Initial Implementation
+ */
 public class BFS {
+
+
 }
--- src/DFS.java
+++ src/DFS.java
@@ -1,2 +1,6 @@
+/**
+ * @author Laura Wysocki and Bryan DeFaria
+ * @version 1.0.0 Initial Implementation
+ */
 public class DFS {
 }
