b18764190297296a97e716b31c51299d8408e468
Phoenix <bryandefaria7@gmail.com>
2023-03-02 09:23:42 -0500

    Hello

--- src/DFS.java
+++ src/DFS.java
@@ -1,6 +1,26 @@
+
+
 /**
  * @author Laura Wysocki and Bryan DeFaria
  * @version 1.0.0 Initial Implementation
  */
 public class DFS {
+
+	public static void main( String[] args )
+		{
+			boolean[] node = new boolean[7];
+			
+			int[][] adj_matrix = {
+						//	 A B C D E F G
+							{0,1,0,1,1,0,0}, // A
+							{1,0,1,0,0,0,0}, // B
+							{0,1,0,1,0,0,0}, // C
+							{1,0,1,0,0,0,0}, // D
+							{1,0,0,0,0,1,1}, // E
+							{0,0,0,0,1,0,1}, // F
+							{0,0,0,0,1,1,0}  // G
+				};
+			
+		}
+
 }
